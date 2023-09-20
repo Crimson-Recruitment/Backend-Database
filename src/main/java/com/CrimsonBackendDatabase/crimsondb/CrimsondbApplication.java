69316bbff1348dabb26e265a058573980ccd97fe
Ssali Benjamin <benjamin.tamale@coseke.com>
2023-09-20 15:40:26 +0300

    First commit

---
+++ src/main/java/com/CrimsonBackendDatabase/crimsondb/CrimsondbApplication.java
@@ -0,0 +1,13 @@
+package com.CrimsonBackendDatabase.crimsondb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrimsondbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrimsondbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CrimsonBackendDatabase/crimsondb/CrimsondbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CrimsonBackendDatabase.crimsondb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrimsondbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
