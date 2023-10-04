d9db772b52a8ffcb19888f0981c79c768404e0c5
Ssali Benjamin <benjamin.tamale@coseke.com>
2023-10-04 16:21:56 +0300

    Added meeting endpoint

--- src/main/java/com/CrimsonBackendDatabase/crimsondb/CrimsondbApplication.java
+++ src/main/java/com/CrimsonBackendDatabase/crimsondb/CrimsondbApplication.java
@@ -2,9 +2,11 @@ package com.CrimsonBackendDatabase.crimsondb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication()
+@EnableAsync
 public class CrimsondbApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/CrimsonBackendDatabase/crimsondb/Meetings/MeetingsService.java
+++ src/main/java/com/CrimsonBackendDatabase/crimsondb/Meetings/MeetingsService.java
@@ -1,19 +1,37 @@
 package com.CrimsonBackendDatabase.crimsondb.Meetings;
 
 import com.CrimsonBackendDatabase.crimsondb.Utils.MeetingInfo;
+import com.google.gson.Gson;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.net.URI;
+import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.time.Duration;
 import java.util.HashMap;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 public class MeetingsService {
+    @Async
     public HashMap<String, String> scheduleMeeting(MeetingInfo meetingInfo){
         System.out.println(meetingInfo.toString());
+        HttpRequest request = HttpRequest
+                .newBuilder(URI.create("https://api.zoom.us/v2/users/ssalibenjamin0402@gmail.com/meetings"))
+                .timeout(Duration.ofHours(1))
+                .header("Content-Type", "application/json")
+                .header("Authorization","5jhxrZRWAofHJTAQyQsQsS_yATCXgW5RQ")
+                .POST(HttpRequest.BodyPublishers.ofString(new Gson().toJson(meetingInfo)))
+                .build();
+        CompletableFuture<Void> response = HttpClient.newHttpClient()
+                .sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::statusCode)
+                .thenAccept(System.out::println);
         HashMap<String,String> result = new HashMap<String, String>();
         result.put("result","success");
+        result.put("more",response.toString());
         return result;
     };
 
