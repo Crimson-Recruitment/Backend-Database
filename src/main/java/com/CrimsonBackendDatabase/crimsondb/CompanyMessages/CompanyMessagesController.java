236d7508d225206d37b0a12237f4a261c08346fa
Ssali Benjamin <benjamin.tamale@coseke.com>
2023-10-26 15:55:41 +0300

    Configured service

--- src/main/java/com/CrimsonBackendDatabase/crimsondb/CompanyMessages/CompanyMessagesController.java
+++ src/main/java/com/CrimsonBackendDatabase/crimsondb/CompanyMessages/CompanyMessagesController.java
@@ -1,31 +1,56 @@
 package com.CrimsonBackendDatabase.crimsondb.CompanyMessages;
 
+import com.CrimsonBackendDatabase.crimsondb.UserMessages.UserMessagesException.InvalidReceiverException;
+import com.CrimsonBackendDatabase.crimsondb.UserToken.UserTokenExceptions.InvalidTokenException;
 import com.CrimsonBackendDatabase.crimsondb.Utils.ChatMessage;
+import org.apache.http.HttpHeaders;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.*;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 @Controller
 public class CompanyMessagesController {
 
     private final CompanyMessagesService companyMessagesService;
+    private final SimpMessageSendingOperations messagingTemplate;
 
     @Autowired
-    public CompanyMessagesController(CompanyMessagesService companyMessagesService) {
+    public CompanyMessagesController(CompanyMessagesService companyMessagesService, SimpMessageSendingOperations messagingTemplate) {
         this.companyMessagesService = companyMessagesService;
+        this.messagingTemplate = messagingTemplate;
     }
 
-    @MessageMapping("/crimsonws/send-message/{receiverType}/{receiverId}")
+    @MessageMapping("/send-message/{receiverType}/{receiverId}")
     @SendTo("/msg/receive-message/{receiverType}/{receiverId}")
-    public ChatMessage sendMessage(
+    public List<String> sendMessage(
             @Payload ChatMessage message,
-            SimpMessageHeaderAccessor headerAccessor,
+            @Headers Map<String, Object> messageHeaders,
             @DestinationVariable Long receiverId,
             @DestinationVariable String receiverType) {
-        String accessToken = (String) headerAccessor.getSessionAttributes().get("username");
-      return message;
+        Map<String, List<String>> headers = (Map<String, List<String>>) messageHeaders.get("nativeHeaders");
+        String accessToken = headers.get(HttpHeaders.AUTHORIZATION).get(0);
+        try {
+            return companyMessagesService.postCompanyMessage(accessToken,message,receiverId,receiverType);
+        } catch (InvalidTokenException | InvalidReceiverException e) {
+            HashMap<String, String> err = new HashMap<String, String>();
+            err.put("error",e.getMessage());
+            messagingTemplate.convertAndSend("/msg/comp/err/ED3", err);
+            throw new RuntimeException(e);
+        }
+    }
+
+    @SendTo("/msg/comp/err/{messageToken}")
+    public HashMap<String,String> sendError(HashMap<String, String> err){
+        return err;
     }
 }
--- src/main/java/com/CrimsonBackendDatabase/crimsondb/CompanyMessages/CompanyMessagesService.java
+++ src/main/java/com/CrimsonBackendDatabase/crimsondb/CompanyMessages/CompanyMessagesService.java
@@ -11,12 +11,12 @@ import com.CrimsonBackendDatabase.crimsondb.UserToken.UserTokenExceptions.Invali
 import com.CrimsonBackendDatabase.crimsondb.Users.Users;
 import com.CrimsonBackendDatabase.crimsondb.Users.UsersRepository;
 import com.CrimsonBackendDatabase.crimsondb.Utils.ChatMessage;
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.HashMap;
-import java.util.Objects;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 public class CompanyMessagesService {
@@ -34,23 +34,32 @@ public class CompanyMessagesService {
         this.userMessagesRepository = userMessagesRepository;
         this.companyMessagesRepository = companyMessagesRepository;
     }
-    public ChatMessage postCompanyMessage(String accessToken, ChatMessage message, Long receiverId, String receiverType) throws InvalidTokenException, InvalidReceiverException {
+    @Transactional
+    public List<String> postCompanyMessage(String accessToken, ChatMessage message, Long receiverId, String receiverType) throws InvalidTokenException, InvalidReceiverException {
         Optional<CompanyToken> companyToken = companyTokenService.findCompanyToken(accessToken);
         if(companyToken.isPresent()) {
             boolean isValid = companyTokenService.validateToken(accessToken, String.valueOf(companyToken.get().getCompany().getId()));
             if(isValid) {
                 Optional<CompanyMessages> userPrimary = companyMessagesRepository.findCompanyMessagesByCompany(companyToken.get().getCompany(), receiverId);
                 if(userPrimary.isPresent()) {
-
+                    List<String> list = userPrimary.get().getMessageArray();
+                    list.add(new Gson().toJson(message));
+                    return list;
                 } else {
                     if(Objects.equals(receiverType, "User")) {
                         Optional<Users> userReceiver = usersRepository.findById(receiverId);
                         if(userReceiver.isPresent()) {
                             Optional<UserMessages> userSecondary = userMessagesRepository.findUserMessagesByUser(userReceiver.get(), companyToken.get().getCompany().getId());
                             if(userSecondary.isPresent()) {
-
+                                List<String> list = userSecondary.get().getMessageArray();
+                                list.add(new Gson().toJson(message));
+                                return list;
                             } else {
-
+                                ArrayList<String> msgList = new ArrayList<String>();
+                                msgList.add(new Gson().toJson(message));
+                                CompanyMessages companyMessages = new CompanyMessages(msgList,receiverType,receiverId,companyToken.get().getCompany());
+                                companyMessagesRepository.save(companyMessages);
+                                return msgList;
                             }
                         } else {
                             throw new InvalidReceiverException("Receiver id is invalid");
@@ -60,25 +69,30 @@ public class CompanyMessagesService {
                         if(companyReceiver.isPresent()) {
                             Optional<CompanyMessages> userSecondary = companyMessagesRepository.findCompanyMessagesByCompany(companyReceiver.get(), receiverId);
                             if(userSecondary.isPresent()) {
-
+                                List<String> list = userSecondary.get().getMessageArray();
+                                list.add(new Gson().toJson(message));
+                                return list;
                             } else {
-
+                                ArrayList<String> msgList = new ArrayList<String>();
+                                msgList.add(new Gson().toJson(message));
+                                CompanyMessages companyMessages = new CompanyMessages(msgList,receiverType,receiverId,companyToken.get().getCompany());
+                                companyMessagesRepository.save(companyMessages);
+                                return msgList;
                             }
                         }else {
                             throw new InvalidReceiverException("Receiver id is invalid");
                         }
+                    } else {
+                        throw new InvalidReceiverException("Receiver type is invalid");
                     }
                 }
-                HashMap<String, String> data = new HashMap<String, String>();
-                data.put("result", "success");
-                return message;
             } else {
                 throw new InvalidTokenException();
             }
         } else {
             throw new InvalidTokenException();
         }
-    };
+    }
     public HashMap<String, String> clearCompanyMessages(String accessToken, Long receiverId, String receiverType) throws InvalidReceiverException, InvalidTokenException {
         Optional<CompanyToken> companyToken = companyTokenService.findCompanyToken(accessToken);
         if(companyToken.isPresent()) {
