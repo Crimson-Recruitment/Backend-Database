1ef6544f7faabe0760192f041d85a2e66248a49b
Ssali Benjamin <benjamin.tamale@coseke.com>
2023-10-27 14:20:36 +0300

    Configuring notifications

---
+++ src/main/java/com/CrimsonBackendDatabase/crimsondb/Notifcations/Notifications.java
@@ -0,0 +1,13 @@
+package com.CrimsonBackendDatabase.crimsondb.Notifcations;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Notifications {
+    private String title;
+    private String message;
+}
---
+++ src/main/java/com/CrimsonBackendDatabase/crimsondb/Notifcations/NotificationsController.java
@@ -0,0 +1,19 @@
+package com.CrimsonBackendDatabase.crimsondb.Notifcations;
+
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class NotificationsController {
+
+    @SendTo("/payments/{paymentId}/{id}")
+    public Notifications sendPaymentNotification(@DestinationVariable Long paymentId,@DestinationVariable Long id, Notifications notification) {
+        return notification;
+    }
+
+    @SendTo("msg/{receiverType}/{receiverId}")
+    public Notifications sendMessageNotification(@DestinationVariable String receiverType,@DestinationVariable Long receiveId, Notifications notification) {
+        return notification;
+    }
+}
